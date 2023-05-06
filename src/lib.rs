71891d0038fab1dbafad42eedede3674c5588d22
ddd <doute.d@gmail.com>
2023-05-06 17:26:08 -0400

    no wasm32

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 #![allow(unused_doc_comments)]
 #![allow(non_upper_case_globals)]
 
+#[cfg(not(target_arch = "wasm32"))]
 mod print;
+#[cfg(not(target_arch = "wasm32"))]
 mod setup;
+#[cfg(not(target_arch = "wasm32"))]
 mod try_zome_call;
+#[cfg(not(target_arch = "wasm32"))]
 mod sweeter_cell;
 
-
+#[cfg(not(target_arch = "wasm32"))]
 pub use print::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use setup::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use try_zome_call::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use sweeter_cell::*;
 
 //----------------------------------------------------------------------------------------
